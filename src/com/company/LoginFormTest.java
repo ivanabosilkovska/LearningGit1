8f8d390125e3204d895536743f43df0c49136acc
Ivana <bosilkovska.ivana@gmail.com>
2022-06-15 19:35:41 +0200

    added new changes

--- src/com/company/LoginFormTest.java
+++ src/com/company/LoginFormTest.java
@@ -5,6 +5,7 @@ public class LoginFormTest {
     public static void main(String[] args) {
 
         System.out.println("Automation test for login form");
+        System.out.println("New changes");
 
     }
 }
